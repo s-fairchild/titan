83efb901a49311e83b89b9b5c6358c98b17caaa7
s-fairchild <steve51516@gmail.com>
2023-09-06 01:55:57 -0400

    Map webpassword to pihole container via volume mount
    WEBPASSWORD isn't taking effect for some reason, attempting to use a file password instead.

--- k3d/scripts/server_pre_install.sh
+++ k3d/scripts/server_pre_install.sh
@@ -8,6 +8,8 @@ systemctl enable --now firewalld
 firewall-cmd --permanent --add-service=kube-apiserver
 firewall-cmd --permanent --add-service=https
 firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-forward-port=port=53:proto=tcp:toport=32053:toaddr=10.50.0.2
+firewall-cmd --permanent --add-forward-port=port=53:proto=udp:toport=32053:toaddr=10.50.0.2
 firewall-cmd --reload
 
 # Required for jellyfin filesystem watcher
