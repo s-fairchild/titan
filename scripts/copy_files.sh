16a6f4dea912c32aa1f208fbb4d2f0ed54a77aac
s-fairchild <steve51516@gmail.com>
2023-09-20 04:08:24 -0400

    Functional k3s single server, 2 agent working configuration
    The iptable forwarding rules only work when attached to the host network. Unless a podman network is attached to the server node, allowing the iptable rules to forward traffic to this network.

---
+++ scripts/copy_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copy k3s files to remote server
+
+set -o nounset
+
+remote_user="${REMOTE_USER}"
+remote_host="${REMOTE_HOST}"
+connection_string="${remote_user}@${remote_host}"
+
+copy_files() {
+    scp "${1}" "${connection_string}:${2:-}"
+}
+
+copy_files k3s_install.env
+copy_files scripts/server_pre_install.sh
+copy_files node/k3s-server-0.service /usr/lib/systemd/system/
+copy_files node/k3s-agent-0.service /usr/lib/systemd/system/
+copy_files node/k3s-server-1.service /usr/lib/systemd/system/
+copy_files node/k3s-server-1.yaml /usr/local/etc/k3s/
+copy_files node/k3s-agent-0.yaml /usr/local/etc/k3s/
+copy_files node/k3s-server-0.yaml /usr/local/etc/k3s/
