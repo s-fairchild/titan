94c2959315a541bb08898f56a66d726a618342e4
s-fairchild <steve51516@gmail.com>
2024-06-21 19:07:16 -0400

    Move butane and ignition variables into their own .env file

--- hack/generate_ignition.sh
+++ hack/generate_ignition.sh
@@ -55,47 +55,13 @@ gen_ignition() {
             "$butane_file" > "$ignition_file"
 }
 
-# Directories
-declare -r deploy_dir="deploy"
-declare -r config_dir="$deploy_dir/config"
-declare ignition_dir="$deploy_dir/ignition"
-
-# Production butane files
-declare -r butane_users="$config_dir/users.bu"
-declare -r butane_systemd="$config_dir/systemd.bu"
-declare -r butane_storage="$config_dir/storage.bu"
-declare -r butane_cluster="$config_dir/cluster.bu"
-# Production ignition files
-declare -r ignition_users="$ignition_dir/users.ign"
-declare -r ignition_systemd="$ignition_dir/systemd.ign"
-declare -r ignition_storage="$ignition_dir/storage.ign"
-declare -r ignition_cluster="$ignition_dir/cluster.ign"
-
-# Development butane files
-declare -r butane_storage_dev="$config_dir/storage-dev.bu"
-declare -r butane_cluster_dev="$config_dir/cluster-dev.bu"
-# Development ignition files
-declare -r ignition_storage_dev="$ignition_dir/storage-dev.ign"
-declare -r ignition_cluster_dev="$ignition_dir/cluster-dev.ign"
-
-declare -rA dev_files=(
-    ["$butane_systemd"]="$ignition_systemd"
-    ["$butane_users"]="$ignition_users"
-    ["$butane_storage_dev"]="$ignition_storage_dev"
-)
-
-declare -rA dev_final=(
-    ["$butane_cluster_dev"]="$ignition_cluster_dev"
-)
-
-declare -rA prod_files=(
-    ["$butane_systemd"]="$ignition_systemd"
-    ["$butane_users"]="$ignition_users"
-    ["$butane_storage"]="$ignition_storage"
-)
-
-declare -rA prod_final=(
-    ["$butane_cluster"]="$ignition_cluster"
-)
+declare -r butane_ignition_files="hack/butane_ignition_files.env"
+if [ -f "$butane_ignition_files" ]; then
+    # shellcheck source=hack/butane_ignition_files.env
+    source "$butane_ignition_files"
+else
+    echo "Missing $butane_ignition_files, aborting"
+    exit 1
+fi
 
 main "$@"
