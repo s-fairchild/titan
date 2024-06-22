456c4a235de3636e1d75f34f5c1635a153356304
s-fairchild <steve51516@gmail.com>
2024-06-22 10:11:17 -0400

    Add disabled package list for kube manifest generation

--- hack/gen_kube_manifests.sh
+++ hack/gen_kube_manifests.sh
@@ -21,20 +21,39 @@ generate_kube_manifests() {
 
     kustomize="$(find "$base" -name kustomization.yaml)"
 
-
     # shellcheck disable=SC2068
     for k in ${kustomize[@]}; do
         kustomize_source="$(dirname "$k")"
-        manifest_name="${kustomize_source##*/}.yaml"
-        log "Generating $manifest_name from $kustomize_source"
-        kubectl kustomize "$kustomize_source" > "$dest/$manifest_name"
+        manifest_name="${kustomize_source##*/}"
+        if is_disabled manifest_name \
+                       disabled_pkgs; then
+            manifest_name+=".yaml"
+            log "Generating $manifest_name from $kustomize_source"
+            kubectl kustomize "$kustomize_source" > "$dest/$manifest_name"
+        fi
     done
+}
+
+is_disabled() {
+    local -n pkg="$1"
+    local -n disabled="$2"
+    if [[ $pkg =~ ${disabled[*]} ]]; then
+        return 1
+    fi
+
+    return 0
 }
 
 declare -r utils="hack/utils.sh"
 if [ -f "$utils" ]; then
     # shellcheck source=utils.sh
     source "$utils"
+fi
+
+declare -r env_file="hack/env/gen_kube_manifests.env"
+if [ -f "$env_file" ]; then
+    # shellcheck source=env/gen_kube_manifests.env
+    source "$env_file"
 fi
 
 main "$@"
