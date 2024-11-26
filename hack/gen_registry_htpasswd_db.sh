bc8496c789c70f434a1a3fb51dba464ebae6e9a4
s-fairchild <steve51516@gmail.com>
2024-11-26 15:56:12 -0500

    Add yes/no prompt and default connection display to htpasswd secret creation
    Shows which remote host the secret will be created on.
    Allows user to confirm action or abort before replacing an existing secret.

--- hack/gen_registry_htpasswd_db.sh
+++ hack/gen_registry_htpasswd_db.sh
@@ -18,6 +18,12 @@ main() {
     create_secret secret_file "registry-auth"
 }
 
+# generate_htpasswd_db()
+# generates a htpasswd database and stores in memory by assigning to out
+# Args
+# 1) user - string
+# 2) password - string
+# 3) out - nameref; empty, htpasswd is assigned to out
 generate_htpasswd_db() {
     user="$1"
     password="$2"
@@ -29,15 +35,28 @@ generate_htpasswd_db() {
     out="$(htpasswd -bBn "${user}" "${password}")"
 }
 
+# get_podman_default_connection()
+# Displays the default podman connection in json
+get_podman_default_connection() {
+    podman system connection list --format=json | jq -r '.[] | select(.Default==true)'
+}
+
+# create_secret()
+# Create podman secret on default remote connection
+# Any existing secret with the same name is replaced, user is prompted for confirmation
+# Args
+# 1) secret - string; Podman secret contents
+# 2) secret_name - string; Podman secret name
 create_secret() {
     local -n secret="$1"
     local secret_name="$2"
     log "starting"
     log "Attempting to create podman secret $secret_name now"
-    # TODO add a prompt to continue or exit here
-    log "WARNING: any existing secret named $secret_name will be replaced"
+    log "WARNING: any existing secret named $secret_name will be replaced on default connection:"
+    get_podman_default_connection
+    prompt_yes_no
 
-    if podman secret create --replace "$secret_name" - <<< "$secret"; then
+    if podman -r secret create --replace "$secret_name" - <<< "$secret"; then
         log "Successfully created podman secret $secret_name"
     fi
 }
